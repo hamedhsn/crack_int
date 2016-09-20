1a15fa48faf085afcb35921275cb412f82cdf7fb
Hamed Saljooghinejad <hamed@erevalue.com>
2016-09-20 08:14:33 +0100

    add 1.7 solution

---
+++ ch1/1.7.py
@@ -0,0 +1,24 @@
+''' Rotate Matrix '''
+
+
+# complexity is O(n^2) - space complexity is 2n^2
+def rotate_matrix(a, n):
+    if len(a) != n*n:
+        print('matrix is not n*n')
+        exit()
+
+    b = [0 for i in range(n*n)]
+    sit_pos = n-1
+    gap = 0
+    for i in range(n*n):
+        if i % n == 0 and i != 0:
+            sit_pos -= 1
+            gap = 0
+
+        b[sit_pos+gap] = a[i]
+        gap += n
+
+    return ''.join(b)
+
+if __name__ == '__main__':
+    print(find('abcdefghijklmnop', 4))
