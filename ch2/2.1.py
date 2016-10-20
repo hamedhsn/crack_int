965130f6bcee3271e6bb852029247a1d588a8551
Hamed Saljooghinejad <hamed@erevalue.com>
2016-10-20 07:43:37 +0100

    add other solution for 2.1 - not using extra DS

--- ch2/2.1.py
+++ ch2/2.1.py
@@ -1,10 +1,14 @@
 # ''' Remove Duplicates '''
 
 from ch2.linkedlist import LinkedList
+import copy
 
 
-# remove duplicates using extra data structure
-def remove_dups(llist):
+def remove_dups_using_dict(llist):
+    """ Remove duplicates using extra data structure(dictionary) - complexity O(N)
+
+    :param llist: linked list of items
+    """
     dup_dict = dict()
     for h in llist.traverse():
         dup_dict[h.data] = dup_dict.get(h.data, 0) + 1
@@ -30,8 +34,32 @@ def remove_dups(llist):
 
     llist.print_all()
 
+
+def remove_dups_wout_ds(llist):
+    """ Remove duplicates without using two pointers - complexity O(N^2)
+
+
+    :param llist: linked list of items
+    """
+    sec_llist = copy.deepcopy(llist)  # avoid two pointer mixing with each other using same reference
+    for item in llist.traverse():
+        if item.next:
+            sec_llist.head = item.next
+
+        prev_link = item
+        for node in sec_llist.traverse():
+            if item.data == node.data:
+                prev_link.next = node.next
+            else:
+                prev_link = node
+
 if __name__ == '__main__':
     llist = LinkedList()
-    llist.append_few([1, 1, 2, 3, 4, 1, 2, 5, 6, 5])
+    llist.append_few([1, 5, 2, 3, 4, 1, 2, 5, 6, 5, 1])
     llist.print_all()
-    remove_dups(llist)
+
+    remove_dups_wout_ds(llist)
+    llist.print_all()
+
+    remove_dups_using_dict(llist)
+    llist.print_all()
