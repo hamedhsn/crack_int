7a5705a9662fe1fbc82b7e5976ef388913c43e0b
Hamed Saljooghinejad <hamed@erevalue.com>
2016-10-24 08:16:42 +0100

    recursive soslution which returns value for 2.2

--- ch2/2.2.py
+++ ch2/2.2.py
@@ -5,7 +5,7 @@ from ch2.linkedlist import LinkedList
 
 # using two pointer
 def find_kth(linked_list, k):
-    """ using two moving pointer in linked list - complexity O(N)
+    """ using two moving pointer in linked list - time complexity O(N)
 
     :param linked_list: input linked list
     :param k: integer k
@@ -42,19 +42,53 @@ def kth_rec(llist_node, k):
 
 
 def find_kth_recursive(llist, k):
-    """
+    """ using recursive approach which prints the results - time complexity is O(N)
+    but space complexity is also O(N)
 
-    :param llist:
-    :param k:
+    :param llist: input linked list
+    :param k: k integer
     """
     kth_rec(llist.head, k)
+
+
+def kth_rec_2(llist_node, k):
+    if llist_node is None:
+        return 0, None
+
+    indx, val = kth_rec_2(llist_node.next, k)
+    indx += 1
+
+    if indx == k:
+        val = llist_node.data
+        # print('Kth item from the end of linked list is : {}'.format(llist_node.data))
+
+    return indx, val
+
+
+def find_kth_recursive_2(llist, k):
+    """ using recursive approach which returns value - time complexity is O(N)
+    but space complexity is also O(N)
+
+    :param llist: input linked list
+    :param k: k integer
+    :return: kth item from end of linked list
+    """
+    indx, val = kth_rec_2(llist.head, k)
+    return val
 
 if __name__ == '__main__':
     linked_list = LinkedList()
     linked_list.append_few([1, 5, 2, 3, 4, 1, 2, 5, 6, 5, 1])
     linked_list.print_all()
 
-    k = find_kth(linked_list, 7)
-    print('\nKth item from the end of linked list is : {}'.format(k))
+    k = 7
+    # method1
+    val = find_kth(linked_list, k)
+    print('\nKth item from the end of linked list is : {}'.format(val))
 
-    find_kth_recursive(linked_list, 7)
+    # method 2
+    find_kth_recursive(linked_list, k)
+
+    # method 3
+    val = find_kth_recursive_2(linked_list, k)
+    print('\nKth item from the end of linked list is : {}'.format(val))
