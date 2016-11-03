c9af39e91b7256abb667cf5f2dae50d9931a3a1c
Hamed Saljooghinejad <hamed@erevalue.com>
2016-11-03 08:01:31 +0000

    add 2.6 solution

--- ch2/2.6.py
+++ ch2/2.6.py
@@ -2,22 +2,25 @@ from ch2.linkedlist import LinkedList, Node
 
 
 def palindrome(ll1):
-    """ find palindrome of a linked list
+    """ find palindrome of a linked list using a recreation of first list in reverse order
     time complexity: O(N)
 
     :param ll1: linked list target
     :return: True if it is palindrome False otherwise
     """
+    if ll1.head is None:
+        print('Linked list is empty')
+        return False
     ll2 = reverse_copy(ll1)
 
     return is_equal(ll1.head, ll2.head)
 
 
-def reverse_copy(lined_list):
+def reverse_copy(linked_list):
     ll2 = LinkedList()
 
-    for item in lined_list.traverse():
-        if item == lined_list.head:
+    for item in linked_list.traverse():
+        if item == linked_list.head:
             n = Node(item.data)
             continue
 
@@ -41,7 +44,57 @@ def is_equal(l1, l2):
     return True
 
 
+# ##### Second approach ######
+def palindrome_runner(linked_list):
+    """ use second runner to solve the problem
+
+    :param linked_list:
+    :return:
+    """
+    is_mid = False
+    odd_len = False
+    stack = list()
+
+    for item in linked_list.traverse():
+
+        if is_mid is False:
+
+            # add head
+            if item == linked_list.head:
+                if item.next is None:
+                    return True
+
+                elif item.next.next is None:
+                    scnd = item
+
+                else:
+                    scnd = item.next.next
+                    stack.append(item.data)
+                    continue
+
+            if scnd.next:
+                stack.append(item.data)
+                if scnd.next.next:
+                    scnd = scnd.next.next
+                else:
+                    is_mid = True
+            else:
+                is_mid = True
+        else:
+            cmpr_dt = stack.pop()
+            if item.data != cmpr_dt:
+                return False
+
+    return True
+
 if __name__ == '__main__':
     ll1 = LinkedList()
-    ll1.create([1, 2, 4, 2, 1])
-    print('\n Palindrome check is {}'.format(palindrome(ll1)))
+
+    ll1.create([])
+    print('\n Palindrome check is: {}'.format(palindrome(ll1)))
+
+    # ll1.create([1, 2, 4, 2, 1])
+    ll1.create([1, 2, 4, 3, 1])
+    print('\n Palindrome check is: {}'.format(palindrome(ll1)))
+
+    print('\n Palindrome check using 2nd method is: {}'.format(palindrome_runner(ll1)))
