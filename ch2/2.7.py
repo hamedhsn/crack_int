de40f88b2f8cf8a4bf2aed98595c69902a45052b
Hamed Saljooghinejad <hamed@erevalue.com>
2016-11-14 23:10:12 +0000

    add second silution for 2.7 with o(1) space- o(n1+n2) complexity

--- ch2/2.7.py
+++ ch2/2.7.py
@@ -3,7 +3,7 @@ import copy
 from ch2.linkedlist import reverse_copy, LinkedList
 
 
-def find_intersection(link_list_1, link_list_2):
+def find_intersection_reverse_traverse(link_list_1, link_list_2):
     """ find the intersection node of the two linked list - time and space complexity is O(N)
 
     :param link_list_1: first linked list
@@ -19,10 +19,12 @@ def find_intersection(link_list_1, link_list_2):
     for n2 in link_list_2.traverse():
         stack2.append(n2)
 
+    # if the last item is not common means they are not intersected
     if n1 != n2:
         print('\nThey are not intersected')
         return None
     else:
+        # traverse to find the intersection node
         target_node = None
         while len(stack1) and len(stack1):
             tmp1 = stack1.pop()
@@ -34,6 +36,46 @@ def find_intersection(link_list_1, link_list_2):
                 return target_node
 
         return target_node
+
+
+# Second solution
+def find_intersection_ignore_extras(link_list_1, link_list_2):
+    """ find the intersection node of the two linked list - time is O(N) but space complexity is O(1)
+
+    :param link_list_1:
+    :param link_list_2:
+    :return:
+    """
+    for no1, n1 in enumerate(link_list_1.traverse()):
+        pass
+
+    for no2, n2 in enumerate(link_list_2.traverse()):
+        pass
+
+    # if the last item is not common means they are not intersected
+    if n1 != n2:
+        print('\nThey are not intersected')
+        return None
+    else:
+        ll1 = link_list_1.head
+        ll2 = link_list_2.head
+
+        if no1 > no2:
+            for i, node in enumerate(link_list_1.traverse(), 1):
+                if i == no1 - no2:
+                    ll1 = node.next
+                    break
+        elif no1 < no2:
+            for i, node in enumerate(link_list_2.traverse(), 1):
+                if i == no2 - no1:
+                    ll2 = node.next
+                    break
+
+        while ll1 != ll2:
+            ll1 = ll1.next
+            ll2 = ll2.next
+
+        return ll1
 
 
 if __name__ == '__main__':
@@ -51,7 +93,7 @@ if __name__ == '__main__':
 
     ll2.print_all()
     print('is list 2')
-    target = find_intersection(ll1, ll2)
+    target = find_intersection_reverse_traverse(ll1, ll2)
     print('\nCommon Node has value : {}'.format(target.data))
 
     # Second test
@@ -64,7 +106,7 @@ if __name__ == '__main__':
     ll3.print_all()
     print('is list 3')
 
-    target = find_intersection(ll3, ll2)
+    target = find_intersection_reverse_traverse(ll3, ll2)
     print('\nCommon Node has value : {}'.format(target.data))
 
     # Third test
@@ -72,6 +114,16 @@ if __name__ == '__main__':
     ll4.create([11, 12, 13])
     ll3.print_all()
     print('is list 4')
-    find_intersection(ll3, ll4)
+    find_intersection_reverse_traverse(ll3, ll4)
 
+    # TEST SECOND METHOD
+    target = find_intersection_ignore_extras(ll1, ll2)
+    print('ll1 intersection with ll2 on data node: {}'.format(target.data))
 
+    target = find_intersection_ignore_extras(ll1, ll3)
+    print('ll1 intersection with ll3 on data node: {}'.format(target.data))
+
+    target = find_intersection_ignore_extras(ll2, ll3)
+    print('ll2 intersection with ll3 on data node: {}'.format(target.data))
+
+    target = find_intersection_ignore_extras(ll4, ll3)
